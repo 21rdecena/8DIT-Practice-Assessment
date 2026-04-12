40f211945a8565dc69d254f110971df4c284a671
21rdecena <21rdecena@wakatipu.school.nz>
2026-04-12 21:31:25 +1200

    switching frames
    added a switching frame function and optimised switching the info label

--- draft.py
+++ draft.py
@@ -1,6 +1,6 @@
 from tkinter import *
 
-class Data:
+class Person:
     def __init__ (self, name, age, has_phone):
         self.name = name
         self.age = age
@@ -11,11 +11,12 @@ class GatherData:
         self.yesno_var = StringVar()
         self.yesno_var.set("yes")
 
+        self.count = 0
         self.person_data = []
 
         # parent
-        info_lb = Label(parent, text="Collecting Person Data", bg='pink', height = 3, wraplength = 200)
-        info_lb.grid(row=0, column=0)
+        self.info_lb = Label(parent, text="Collecting Person Data", bg='pink', height = 3, wraplength = 200)
+        self.info_lb.grid(row=0, column=0)
 
         self.switch_btn = Button(parent, text="Show All", command=self.switch_frames)
         self.switch_btn.grid(row=0, column=1)
@@ -48,43 +49,57 @@ class GatherData:
         self.enter_btn = Button(self.data_frame, text="Enter Data:",  command=self.enter_data)
         self.enter_btn.grid(row=4, column=0, columnspan=2)
        
-    '''
         # displaying data
 
         self.display_frame = Frame(parent)
-        self.display_frame.grid(row=1, column=0, columnspan=2)
 
-        name_lb = Label(self.data_frame, text="First name: ")
+        name_lb = Label(self.display_frame, text="First name: ")
         name_lb.grid(row=0, column=0)
         
-        age_lb = Label(self.data_frame, text="Age: ")
+        age_lb = Label(self.display_frame, text="Age: ")
         age_lb.grid(row=1, column=0)
         
-        phone_lb = Label(self.data_frame, text="Do you have a mobile phone?")
+        phone_lb = Label(self.display_frame, text="Do you have a mobile phone?")
         phone_lb.grid(row=2, column=0)
 
-        self.enter_btn = Button(self.data_frame, text="Enter Data:",  command=self.enter_data)
+        self.enter_btn = Button(self.display_frame, text="Enter Data:",  command=self.enter_data)
         self.enter_btn.grid(row=4, column=0, columnspan=2)
 
-        self.enter_btn = Button(self.data_frame, text="Enter Data:",  command=self.enter_data)
+        self.enter_btn = Button(self.display_frame, text="Enter Data:",  command=self.enter_data)
         self.enter_btn.grid(row=4, column=0, columnspan=2)
-     '''
+
 
 
     def enter_data(self):
         user_first = self.name_entry.get()
         user_age = self.age_entry.get()
         user_phone = self.yesno_var.get()
-        self.person_data.append(Data(user_first, user_age, user_phone))
-        for person in self.person_data:
-            print(person.name)
+        self.person_data.append(Person(user_first, user_age, user_phone))
+        for person in self.person_data: #placeholder, test if code is working
+            print(person.name)     
             print(person.age)
             print(person.has_phone)
-            
-    
-    def switch_frames(self):
-        print("Frames switched")
 
+    def switch_frames(self):
+        if self.count == 0:
+            self.data_frame.grid_forget()
+            self.display_frame.grid(row=1, column=0, columnspan=2)
+            self.switch_info()
+            self.count += 1
+        else:
+            self.display_frame.grid_forget()
+            self.data_frame.grid(row=1, column=0, columnspan=2)
+            self.switch_info()
+            self.count = 0
+
+    def switch_info(self):
+        if self.count == 0:
+            self.info_lb.configure(text="Displaying Person Data")
+            self.switch_btn.configure(text="Add New Person")
+        else:
+            self.info_lb.configure(text="Collecting Person Data")
+            self.switch_btn.configure(text="Show All")
+            
 if __name__ == "__main__":
     root = Tk()
     root.title("Gather Data")
